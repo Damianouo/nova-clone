5628c6fcad07596c74c3c60a9ba7cb59845584a0
Damian <Damian@fakemail.com>
2023-02-17 00:15:58 +0800

    transform a into Link

--- src/Components/Navbar/Navbar.style.js
+++ src/Components/Navbar/Navbar.style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledNavbar = styled.div`
   display: flex;
@@ -33,36 +33,37 @@ export const StyledNavbar = styled.div`
       display: none;
     }
   }
-`;
 
-export const StyledNavLink = styled.a`
-  min-width: 100px;
-  padding: 14px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 14px;
-  color: white;
-  border-bottom: 1px solid white;
-  transition: all 300ms ease;
-  background-color: black;
+  //' Anchor tag(Link)
+  a {
+    min-width: 100px;
+    padding: 14px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    color: white;
+    border-bottom: 1px solid white;
+    transition: all 300ms ease;
+    background-color: black;
 
-  &:hover {
-    filter: drop-shadow(0px 0px 20px var(--color-shadow));
-  }
+    &:hover {
+      filter: drop-shadow(0px 0px 20px var(--color-shadow));
+    }
 
-  svg {
-    width: 16px;
-    height: 16px;
-    margin-bottom: 8px;
-    fill: white;
-    /* stroke: black; */
-  }
+    svg {
+      width: 16px;
+      height: 16px;
+      margin-bottom: 8px;
+      fill: white;
+      /* stroke: black; */
+    }
 
-  @media screen and (max-width: 768px) {
-    min-width: 15vw;
-    border: none;
-    outline: none;
+    @media screen and (max-width: 768px) {
+      min-width: 15vw;
+      border: none;
+      outline: none;
+    }
   }
 `;
